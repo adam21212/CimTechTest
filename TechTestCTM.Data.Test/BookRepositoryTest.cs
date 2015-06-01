33665384581aac9c0e7ea661ca7b4f59597ab0e2
adam21212 <adam.gleed@yahoo.co.uk>
2015-06-01 08:13:42 +0100

    Add Test and implementation for book repository

---
+++ TechTestCTM.Data.Test/BookRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTestCTM.Data.Configuration;
+using System.IO;
+
+namespace TechTestCTM.Data.Test
+{
+    [TestClass]
+    public class BookRepositoryTest
+    {
+        [TestMethod]
+        public void When_given_config_BookRepository_returns_Book()
+        {
+            Mock<IBookRepositoryConfig> config = new Mock<IBookRepositoryConfig>();
+            config.Setup(x => x.Books["42"]).Returns(new TechTestCTM.Data.Configuration.Book()
+            {
+                Id = "42",
+                FilePath = "\\Books\\Test.txt"
+            });
+            var bookRepository = new BookRepository(config.Object);
+            var book = bookRepository.GetById(42);
+            Assert.AreEqual("42", book.Id);
+            Assert.AreEqual("\\Books\\Test.txt", book.FilePath);
+            Assert.AreEqual((new StreamReader(book.Stream)).ReadToEnd(), "The quick fox jumped over the lazy dog.");
+        }
+    }
+}
